32ae51dea71fa3d7678f1e60226a9705e340e9b5
Zabello <Zonderqart@gmail.com>
2016-05-18 23:41:15 +0300

    Сделал унарный плюс

--- Калькулятор/scripts/calculator.js
+++ Калькулятор/scripts/calculator.js
@@ -1,16 +1,30 @@
+function formatNumber(myNumber) {
+    if (myNumber == 0) {
+        alert('В делителе ноль. Недопустимое значение');
+    }
+    return myNumber;
+}
+
+/*function format(myNumber) {
+    if (myNumber == 'NaN') {
+        alert('Проверте ввод значений');
+    }
+    return mygNumber;
+}*/
+
 function ts() {
     var myel = document.getElementById('itog')
     var fun = document.getElementById('fun').value
     var s1 = document.getElementById('s1').value
     var s2 = document.getElementById('s2').value
     if (fun == 1) {
-        fun = s1 * s2
+        fun = +s1 + +s2
     } else if (fun == 2) {
-        fun = s1 / s2;
+        fun = s1 - s2;
     } else if (fun == 3) {
-        fun = s1 + s2
+        fun = s1 / formatNumber(s2);
     } else {
-        fun = s1 - s2
+        fun = s1 * s2
     }
     myel.innerText = fun;
 }
