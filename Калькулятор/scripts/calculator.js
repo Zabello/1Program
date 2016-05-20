5424fdb0b827614e962785eb7b969ed59901a247
Zabello <Zonderqart@gmail.com>
2016-05-20 17:52:15 +0300

    некоректно убрал тройку

--- Калькулятор/scripts/calculator.js
+++ Калькулятор/scripts/calculator.js
@@ -3,25 +3,26 @@ function ts() {
     var fun = document.getElementById('fun').value;
     var s1 = Number(document.getElementById('s1').value);
     var s2 = Number(document.getElementById('s2').value);
+    var fun1 = itog;
 
     if (isNaN(s1) || isNaN(s2)) {
         alert("Проверте параметры ввода");
     } else {
         if (fun == 1) {
-            fun = +s1 + +s2;
+            fun1 = +s1 + +s2;
         } else if (fun == 2) {
-            fun = s1 - s2;
+            fun1 = s1 - s2;
         } else if (fun == 3) {
             if (s2 !== 0) {
-                fun = s1 / s2;
+                fun1 = s1 / s2;
             } else {
                 alert("Делить на ноль нельзя");
             }
         } else {
-            fun = s1 * s2;
+            fun1 = s1 * s2;
         }
 
-        myel.innerText = fun;
+        myel.innerText = fun1;
     }
 }
 // Вопросы:
