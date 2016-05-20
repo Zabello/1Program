7eba22ac50c9ff77d42f9375ae97bbd53ad298b2
Zabello <Zonderqart@gmail.com>
2016-05-20 16:06:49 +0300

    Исправление ошибок. с перечнем вопросов.

--- Калькулятор/scripts/calculator.js
+++ Калькулятор/scripts/calculator.js
@@ -1,30 +1,30 @@
-function formatNumber(myNumber) {
-    if (myNumber == 0) {
-        alert('В делителе ноль. Недопустимое значение');
-    }
-    return myNumber;
-}
-
-/*function format(myNumber) {
-    if (myNumber == 'NaN') {
-        alert('Проверте ввод значений');
-    }
-    return mygNumber;
-}*/
-
 function ts() {
-    var myel = document.getElementById('itog')
-    var fun = document.getElementById('fun').value
-    var s1 = document.getElementById('s1').value
-    var s2 = document.getElementById('s2').value
-    if (fun == 1) {
-        fun = +s1 + +s2
-    } else if (fun == 2) {
-        fun = s1 - s2;
-    } else if (fun == 3) {
-        fun = s1 / formatNumber(s2);
+    var myel = document.getElementById('itog');
+    var fun = document.getElementById('fun').value;
+    var s1 = Number(document.getElementById('s1').value);
+    var s2 = Number(document.getElementById('s2').value);
+
+    if (isNaN(s1) || isNaN(s2)) {
+        alert("Проверте параметры ввода");
     } else {
-        fun = s1 * s2
+        if (fun == 1) {
+            fun = +s1 + +s2;
+        } else if (fun == 2) {
+            fun = s1 - s2;
+        } else if (fun == 3) {
+            if (s2 !== 0) {
+                fun = s1 / s2;
+            } else {
+                alert("Делить на ноль нельзя");
+            }
+        } else {
+            fun = s1 * s2;
+        }
+
+        myel.innerText = fun;
     }
-    myel.innerText = fun;
 }
+// Вопросы:
+// 1. Number- какое имеет значение?
+// 2. Можно ли избавится от результата "3" после деления на "0"?
+// 3. Значение "!==", какую функцию выполняет "!"?
