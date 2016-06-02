5286f7f1b49bbdd99a456943ce35f19db04246ed
Zabello <Zonderqart@gmail.com>
2016-06-02 22:46:51 +0300

    Редакция с костылями и креативными решениями!

--- Калькулятор/scripts/calculator.js
+++ Калькулятор/scripts/calculator.js
@@ -1,3 +1,41 @@
+function validffForm(f) {
+    // Если введено число, то скрываем предупреждение
+    if (isDigit(f.value)) document.getElementById("msg").style.display = "none";
+    // В противном случае отображаем предупреждение
+    else document.getElementById("msg").style.display = "inline";
+}
+// Функция по проверке, число введено или нет
+function isDigit(data) {
+    var numStr = "0123456789";
+    var k = 0;
+    for (i = 0; i < data.length; i++) {
+        thisChar = data.substring(i, i + 1);
+        if (numStr.indexOf(thisChar) != -1) k++;
+    }
+    if (k == data.length) return 1;
+    else return 0;
+}
+
+function validForm(f) {
+    // Если введено число, то скрываем предупреждение
+    if (isDigit(f.value)) document.getElementById("msgq").style.display = "none";
+    // В противном случае отображаем предупреждение
+    else document.getElementById("msgq").style.display = "inline";
+}
+// Функция по проверке, число введено или нет
+function isDigit(data) {
+    var numStr = "0123456789";
+    var k = 0;
+    for (i = 0; i < data.length; i++) {
+        thisChar = data.substring(i, i + 1);
+        if (numStr.indexOf(thisChar) != -1) k++;
+    }
+    if (k == data.length) return 1;
+    else return 0;
+}
+
+
+
 function ts() {
     var myel = document.getElementById('itog');
     var fun = document.getElementById('fun').value;
@@ -6,7 +44,7 @@ function ts() {
     var fun1;
 
     if (isNaN(s1) || isNaN(s2)) {
-        alert("Проверте параметры ввода");
+        // alert("Проверте параметры ввода");
     } else {
         if (fun == 1) {
             fun1 = +s1 + +s2;
@@ -16,7 +54,7 @@ function ts() {
             if (s2 !== 0) {
                 fun1 = s1 / s2;
             } else {
-                alert("Делить на ноль нельзя");
+                // alert("Делить на ноль нельзя");
             }
         } else {
             fun1 = s1 * s2;
